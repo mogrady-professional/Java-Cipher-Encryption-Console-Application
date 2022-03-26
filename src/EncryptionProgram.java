e81f7b44b42e8d69211a5a653fdcceaacefd389f
Michael O'Grady <mogrady.professional@gmail.com>
2022-03-26 15:43:38 +0000

    Initial Commit

---
+++ src/EncryptionProgram.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class EncryptionProgram {
+ 
+	EncryptionProgram() {
+// Constructor
+	}
+
+	private void menu() {
+// Menu
+	}
+
+	private void newKey() {
+// Cipher
+
+	}
+
+	private void getKey() {
+// Key
+	}
+
+	private void encrypt() {
+		System.out.println("Enter a message to be encrypted: ");
+
+	}
+
+	private void decrypt() {
+		System.out.println("Enter a message to be decrypted: ");
+
+	}
+
+	private void quit() {
+		System.out.println("Thank you, for using the Encryption Program.");
+		System.exit(0);
+	}
+}
